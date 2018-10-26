120c3a87e010b94d9eef62953c8a797c308291bc
bfazeli <bfazeli1@gmail.com>
2018-10-25 19:29:18 -0700

    Update: Created an Input comp and used it to create an Email and password input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
-import { View, Text} from 'react-native'
-import { Header } from './Components/Common'
+import { View } from 'react-native'
+import firebase from 'firebase'
+import { Header } from './components/common'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  // Invoke the componentWillMount LCM
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDRi08ItPSKIXA9pBy3jPfOzY_d5XXFM1Q",
+      authDomain: "auth-f4b09.firebaseapp.com",
+      databaseURL: "https://auth-f4b09.firebaseio.com",
+      projectId: "auth-f4b09",
+      storageBucket: "auth-f4b09.appspot.com",
+      messagingSenderId: "209726283659"
+    })
+  }
+
   render() {
     return(
       <View>
         <Header headerText= "Authentication" />
+        <LoginForm />
       </View>
     )
   }
